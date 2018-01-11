d159b550866f2fee6210beabfce34ef13aa01f69
hui.zhao <hui.zhao@ctrip.com>
2018-01-11 14:15:17 +0800

    调整一下日志打印

--- rocket-core/src/main/java/cn/hikyson/rocket/helper/OnCreateAndDependencyParsedCallback.java
+++ rocket-core/src/main/java/cn/hikyson/rocket/helper/OnCreateAndDependencyParsedCallback.java
@@ -1,10 +1,12 @@
 package cn.hikyson.rocket.helper;
 
+import android.app.Activity;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 
 /**
  * Created by kysonchao on 2018/1/5.
  */
 public interface OnCreateAndDependencyParsedCallback {
-    void onCreateAndDependencyParsed(@NonNull String[] dependencies);
+    void onCreateAndDependencyParsed(Activity activity, Bundle savedInstanceState, @NonNull String[] dependencies);
 }
